d7f99bc7f6505160230d8217fb88af7eadf0da5e
ahmed18062005 <117263709+ahmed18062005@users.noreply.github.com>
2022-11-02 05:05:56 -0700

    Add files via upload

---
+++ app.js
@@ -0,0 +1,45 @@
+window.alert("Well Come To Project");
+var a1 = "Muhammad Ahmed Raza";
+var b2 = "Abid Hussain";
+ console.log (a1); 
+ console.log (b2);
+ 
+ var c3 = 20;
+ var d4 = 10;
+ console.log(c3 - d4);
+
+ var e5 = 20;
+ var f6 = 10;
+ console.log(e5 * f6);
+
+ var g7 = 20;
+ var h8 = 10;
+ console.log(g7, h8);
+
+ var i9 = 20;
+ var j10 = 10;
+ console.log(i9 / j10);
+ 
+ var k11 = 20;
+ var l12 = 10;
+ console.log(k11 + l12);
+
+ var m13 = 100;
+ var n14 =m13++;
+ console.log(m13)
+ 
+ var o15 = 100;
+ var p16 =++o15;
+ console.log(o15)
+
+ var q17 = 100;
+ var r18 =--q17;
+ console.log(q17)
+
+ var s19 = 100;
+ var t20 =s19--;
+ console.log(s19)
+
+ var u21 =100;
+ var v22 =10;
+ console.log(u21*v22+u21-v22);
